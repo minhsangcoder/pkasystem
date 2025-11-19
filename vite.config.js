7e896922f3d1b99693880dd7f2eadd1043e2af31
minhsangcoder <minhsangcoder@gmail.com>
2025-11-19 19:31:20 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.PKASYSTEM_BASE_URL || '/pkasystem',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
